f52a154807848a6464d3c15cb7b70314fa165c7c
Kathiu <kathyu1298@gmail.com>
2026-08-18 14:25:21 -0500

    Actualizar caché PWA para publicar sincronización y reporte mejorado

--- sw.js
+++ sw.js
@@ -1,24 +1,13 @@
-const CACHE='inspecciones-v2';
+const CACHE='inspecciones-v3';
 const FILES=['./','./index.html','./styles.css','./app.js','./catalog.json','./manifest.json'];
 
 self.addEventListener('install',event=>{
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache=>cache.addAll(FILES))
-      .then(()=>self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()));
 });
-
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener('fetch',event=>{
-  event.respondWith(
-    caches.match(event.request).then(response=>response||fetch(event.request))
-  );
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));
 });
